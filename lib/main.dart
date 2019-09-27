97659bb88014c1937895f294ddedc74fc79cdeec
Kartik Chauhan <kckartik17@gmail.com>
2019-09-27 21:17:31 +0530

    app setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      statusBarColor: Colors.transparent,
+    ),
+  );
+
+  SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(),
+    );
+  }
+}
